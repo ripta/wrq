25b8b6313f7d3ab1eb44ea79b5947340b202c0bb
Ripta Pasay <ripta@users.noreply.github.com>
2026-07-13 20:10:08 -0700

    Cast to unsigned char before isalpha in print_units
    ctype functions are undefined for a negative char argument. Where char
    is signed, a byte >= 0x80 becomes negative and triggers UB. Cast to
    unsigned char, the standard-correct form.
    
    No behavior change today: the formatted unit strings are ASCII. This
    removes the technical UB.

--- src/wrk.c
+++ src/wrk.c
@@ -797,8 +797,8 @@ static void print_units(long double n, char *(*fmt)(long double), int width) {
     char *msg = fmt(n);
     int len = strlen(msg), pad = 2;
 
-    if (isalpha(msg[len-1])) pad--;
-    if (isalpha(msg[len-2])) pad--;
+    if (isalpha((unsigned char)msg[len-1])) pad--;
+    if (isalpha((unsigned char)msg[len-2])) pad--;
     width -= pad;
 
     printf("%*.*s%.*s", width, width, msg, pad, "  ");
